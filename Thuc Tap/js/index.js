c6e1df1ce78745f9233e72931cdb52a78f7067b2
Nam <nam17081999@gmail.com>
2021-10-29 18:09:47 +0700

    first commit

---
+++ Thuc Tap/js/index.js
@@ -0,0 +1,71 @@
+function showMenu() {
+    var menuStyle = document.querySelector("#nav > ul").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu1() {
+
+    var menuStyle = document.querySelector(".subnav1").style
+
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu2() {
+    var menuStyle = document.querySelector(".subnav2").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu3() {
+    var menuStyle = document.querySelector(".subnav3").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu4() {
+    var menuStyle = document.querySelector(".subnav4").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu5() {
+    var menuStyle = document.querySelector(".subnav5").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
+
+function showMenu6() {
+    var menuStyle = document.querySelector(".subnav6").style
+
+    if (menuStyle.display == 'flex') {
+        menuStyle.display = 'none'
+    } else {
+        menuStyle.display = 'flex'
+    }
+}
